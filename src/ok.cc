3121f47ca7fbae9709d93f24fe383a48019772ab
scott-linder <scott.b.linder@wmich.edu>
2013-11-18 23:14:03 -0500

    Made some critical type sizes a little more concrete.

--- src/ok.cc
+++ src/ok.cc
@@ -1,4 +1,7 @@
-int *gpio_controller = (int *) 0x20200000;
+#include "types.hh"
+#include "ok.hh"
+
+word_t *gpio_controller = (word_t *) 0x20200000;
 
 void okLightOn() {
     gpio_controller[1] = 1 << 18;
---
+++ src/types.hh
@@ -0,0 +1,9 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+typedef std::uint32_t size_t;
+typedef std::uint32_t word_t;
+
+#endif /* TYPES_H */
