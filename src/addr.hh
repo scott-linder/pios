daf103fa57d193c68d04f060b1373590d5129c35
scott-linder <scott.b.linder@wmich.edu>
2013-11-28 20:15:22 -0500

    Changes to types and literals.
    * Corrected casts in addr.hh
    * Added new types and literals for word/byte counts.
    * Added and expanded comments.

--- src/addr.hh
+++ src/addr.hh
@@ -1,25 +1,49 @@
 #ifndef ADDR_HH
 #define ADDR_HH
 
+#include "types.hh"
+
+/**
+ * Literal representing a physical address.
+ */
 constexpr
 reg_t *operator"" _phys(unsigned long long addr) {
-    return (reg_t *) addr;
+    return reinterpret_cast<reg_t *>(addr);
 }
 
+/**
+ * Literal representing a bus address.
+ */
 constexpr
 reg_t *operator"" _bus(unsigned long long addr) {
     /*
      * The spec defines peripherals in terms of bus addresses when we want
      * physical addresses
      */
-    return (reg_t *) (addr - 0x7E000000 + 0x20000000);
+    return reinterpret_cast<reg_t *>(addr - 0x7E000000 + 0x20000000);
+}
+
+/**
+ * Literal representing a byte count.
+ */
+constexpr
+bytes_t operator"" _bytes(unsigned long long bytes) {
+    return static_cast<bytes_t>(bytes);
+}
+
+/**
+ * Literal representing a word count.
+ */
+constexpr
+words_t operator"" _words(unsigned long long words) {
+    return static_cast<words_t>(words);
 }
 
 /**
  * Convert a byte count into a word count.
  */
 constexpr
-auto words(size_t bytes) -> size_t {
+auto words(bytes_t bytes) -> words_t {
     return bytes / sizeof(word_t);
 }
 
@@ -27,7 +51,7 @@ auto words(size_t bytes) -> size_t {
  * Convert a word count into a byte count.
  */
 constexpr
-auto bytes(size_t words) -> size_t {
+auto bytes(words_t words) -> bytes_t {
     return words * sizeof(word_t);
 }
 
--- src/types.hh
+++ src/types.hh
@@ -4,11 +4,17 @@
 #include <cstdint>
 
 typedef std::uint32_t size_t;
-/** The word size of the BCM2835 */
+/** A word count. */
+typedef size_t words_t;
+/** A byte count. */
+typedef size_t bytes_t;
+/** The word size of the BCM2835. */
 typedef std::uint32_t word_t;
 /** The type used to refer to a "mapped io" register.
  * We use a volatile variant to make sure _all_ operations, no matter how
- * uneeded they seem, are guaranteed not to be optimized away.
+ * uneeded they seem, are guaranteed not to be optimized away. This is of
+ * course not helpful for performance, so this type should not be used
+ * _everywhere_ only where appropriate.
  */
 typedef volatile word_t reg_t;
 
