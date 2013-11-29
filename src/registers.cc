430196327e8628987bed7bf6d7446446d82ae75e
scott-linder <scott.b.linder@wmich.edu>
2013-11-28 20:23:36 -0500

    Updated Registers to use proper casts and new types.

--- src/registers.cc
+++ src/registers.cc
@@ -7,41 +7,41 @@ Registers::Registers(reg_t *base)
 
 /** Read a given register in full.
  */
-auto Registers::read(size_t word_offset) -> word_t {
-    return (word_t) base[word_offset];
+auto Registers::read(words_t offset) -> word_t {
+    return static_cast<word_t>(base[offset]);
 }
 
 /** Read a given bit from a given register.
  * XXX: stub
  */
-auto Registers::read(size_t word_offset, int bit) -> bool {
-    return (bool) read(word_offset) & 1 << bit;
+auto Registers::read(words_t offset, int bit) -> bool {
+    return static_cast<bool>(read(offset) & 1 << bit);
 }
 
 /** Read a given portion of a given register.
  * XXX: stub
  */
-auto Registers::read(size_t word_offset, int bit_from, int bit_to) -> word_t {
-    return word_offset + bit_from + bit_to;
+auto Registers::read(words_t offset, int bit_from, int bit_to) -> word_t {
+    return offset + bit_from + bit_to;
 }
 
 /** Write a given register in full.
  */
-auto Registers::write(size_t word_offset, word_t word) -> void {
-    base[word_offset] = word;
+auto Registers::write(words_t offset, word_t word) -> void {
+    base[offset] = word;
 }
 
 /** Write a given bit of a given register.
  * XXX: stub
  */
-auto write(size_t word_offset, int bit, bool bit_status) -> void {
-    word_offset += bit + bit_status;
+auto write(words_t offset, int bit, bool bit_status) -> void {
+    offset += bit + bit_status;
 }
 
 /** Write a given portion of a given register.
  * XXX: stub
  */
-auto write(size_t word_offset, int bit_from, int bit_to, word_t value) -> void {
-    word_offset += bit_from + bit_to + value;
+auto write(words_t offset, int bit_from, int bit_to, word_t value) -> void {
+    offset += bit_from + bit_to + value;
 }
 
