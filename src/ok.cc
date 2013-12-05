32b48fe952b33bc108a0086154add2a8c7445a34
scott-linder <scott.b.linder@wmich.edu>
2013-12-05 16:35:07 -0500

    Moved OK to new MMIO write methods.

--- src/ok.cc
+++ src/ok.cc
@@ -8,12 +8,12 @@
 #include "ok.hh"
 
 auto OK::on() -> void {
-    regs_.write(kSelect1, 1 << 18);
-    regs_.write(kClear0, 1 << 16);
+    regs_.write(kSelect1, 18, true);
+    regs_.write(kClear0, 16, true);
 }
 
 auto OK::off() -> void {
-    regs_.write(kSelect1, 1 << 18);
-    regs_.write(kSet0, 1 << 16);
+    regs_.write(kSelect1, 18, true);
+    regs_.write(kSet0, 16, true);
 }
 
